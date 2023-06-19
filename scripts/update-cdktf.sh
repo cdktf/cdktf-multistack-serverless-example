6dda49cd87b517ad734be477199aefca9bd4441d
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-06-19 10:52:29 +0200

    chore: improve update script

--- scripts/update-cdktf.sh
+++ scripts/update-cdktf.sh
@@ -11,7 +11,16 @@ if [ -z "$CDKTF_VERSION" ]; then
 fi
 
 echo "Updating to cdktf version $CDKTF_VERSION"
+git checkout -b "cdktf-$CDKTF_VERSION"
 cd $PROJECT_ROOT
 
 yarn add -D -W cdktf-cli@$CDKTF_VERSION
 yarn add -W cdktf@$CDKTF_VERSION @cdktf/provider-aws@latest @cdktf/provider-local@latest
+
+git add .
+git commit -m "feat: update to cdktf $CDKTF_VERSION"
+git push origin "cdktf-$CDKTF_VERSION"
+
+gh pr create --fill --base main --head "cdktf-$CDKTF_VERSION" --title "feat: update to cdktf $CDKTF_VERSION" --label "cdktf-update-$CDKTF_VERSION"
+
+
